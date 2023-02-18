2b224692228972f323623b14e9f654c7079c9207
huydeve <haihuy129@gmail.com>
2023-02-18 16:02:43 +0700

    header

---
+++ pages/shipper/[id]/index.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import ShipperOrderDetailHeader from "../../../components/Header/ShipperOrderDetail.header";
+
+type Props = {};
+
+export default function ShipperOrderDetailPage({}: Props) {
+  return (
+    <div>
+      <ShipperOrderDetailHeader />
+    </div>
+  );
+}
--- pages/shipper/order-detail/index.tsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-type Props = {}
-
-export default function ShipperOrderDetailPage({}: Props) {
-  return (
-    <div>ManagementPage</div>
-  )
-}
