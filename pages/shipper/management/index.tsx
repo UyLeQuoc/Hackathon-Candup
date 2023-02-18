1860638a854ba5227766dce53037620e048ac1b6
huydeve <haihuy129@gmail.com>
2023-02-18 15:42:43 +0700

    add shipper route

---
+++ pages/shipper/management/index.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+type Props = {};
+
+export default function ShipperManagementPage({}: Props) {
+  return <div>ManagementPage</div>;
+}
---
+++ pages/shipper/order-detail/index.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+type Props = {}
+
+export default function ShipperOrderDetailPage({}: Props) {
+  return (
+    <div>ManagementPage</div>
+  )
+}
